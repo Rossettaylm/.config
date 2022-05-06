9310a90313452975be2f92e8eafc8f6e07ee7af7
Rossettaylm <2441197035@qq.com>
2022-05-06 12:50:34 +0800

    commit

---
+++ zsh/env.zsh
@@ -0,0 +1,25 @@
+#!/usr/bin/env zsh
+
+
+# If you come from bash you might have to change your $PATH.
+# export PATH=$HOME/bin:/usr/local/bin:$PATH
+#
+export CMAKE_HOME="/usr/local/cmake-3.22.4-linux-x86_64"
+export GDB_HOME="/usr/local/gdb-11.1"
+export TOMCAT_HOME="/usr/share/tomcat8"
+export CLION_PROJECT="/home/rossetta/CLionProjects"
+export GITHUB_ACCOUNT_PREFIX="https://github.com/Rossettaylm"
+
+export PATH=$PATH:$CMAKE_HOME/bin
+export PATH=$PATH:$GDB_HOME/bin
+export PATH=$PATH:/home/rossetta/Code/shell/myscripts
+
+# Path to your oh-my-zsh installation.
+export ZSH_HOME="$HOME/.config/zsh"
+export OHMYZSH="$ZSH_HOME/oh-my-zsh"
+export FZF_HOME="$HOME/Softwares/fzf"
+
+export EDITOR=nvim
+export TERM=xterm-256color
+
+
