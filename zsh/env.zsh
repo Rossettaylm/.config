046da4c038edc4e02558ef4cbfe4022bcad1cad5
Rossettaylm <2441197035@qq.com>
2022-05-06 11:34:49 +0800

    first commit

---
+++ zsh/env.zsh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+
+
+# If you come from bash you might have to change your $PATH.
+# export PATH=$HOME/bin:/usr/local/bin:$PATH
+export GITHUB_ACCOUNT_PREFIX="https://github.com/Rossettaylm"
+
+export PATH=$PATH:/home/rossetta/Code/shell/myscripts
+
+# Path to your oh-my-zsh installation.
+export ZSH_HOME="$HOME/.config/zsh"
+export OHMYZSH="$ZSH_HOME/oh-my-zsh"
+export FZF_HOME="$HOME/Softwares/fzf"
+
+export EDITOR=nvim
+export TERM=xterm-256color
+
+
