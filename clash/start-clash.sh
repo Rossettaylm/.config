08ece74ebe8d650cdac495661826ad496d0ee076
Rossettaylm <2441197035@qq.com>
2022-05-30 03:16:08 +0800

    add clash subscribe url mojie

--- clash/start-clash.sh
+++ clash/start-clash.sh
@@ -7,11 +7,7 @@ CLASH_HOME=$HOME/.config/clash
 CLASH_URL="https://linkuserssnk.xxyjx.cc/link/QqnkZ7iTjyeo1fvM?clash=1"
 
 printf "正在更新clash订阅地址(%s)...\n" ${CLASH_URL}
-diff ${CLASH_HOME}/config.yaml < $(curl -s ${CLASH_URL})
 printf "正在启动服务...\n"
-if [ $? == 0 ]; then
-    /usr/bin/clash -d $CLASH_HOME/ -ext-ctl "0.0.0.0:9090"
-else
-    curl -L -o ${CLASH_HOME}/config.yaml ${CLASH_URL}
-    /usr/bin/clash -d $CLASH_HOME/ -ext-ctl "0.0.0.0:9090"
-fi
+
+curl -L -o ${CLASH_HOME}/config.yaml ${CLASH_URL}
+/usr/bin/clash -d $CLASH_HOME/ -ext-ctl "0.0.0.0:9090"
