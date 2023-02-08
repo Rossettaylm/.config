cc98c1365faf4dea6426d7ecad5a024c9784ef00
Rossettaylm <2441197035@qq.com>
2023-02-08 11:50:12 +0800

    add aliaes

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,8 +2,6 @@
 
 alias ll="ls -lh"
 alias la="ls -a"
-alias setproxy="export http_proxy='http://127.0.0.1:7890' && export https_proxy='https://127.0.0.1:7890'"
-alias unsetproxy="unset http_proxy && unset https_proxy"
 alias e="exit"
 alias sudo="sudo -E"
 alias neo='neofetch'
@@ -15,14 +13,14 @@ alias ezsh="nvim $ZSH_HOME/zshrc"
 alias base="conda activate base"
 alias sra='sudo -E ranger_cd'
 alias vim='nvim'
-alias gg='git clone'
 alias up='sudo pacman -Syyu'
 alias clip='xclip -selection c'
-alias px='proxychains'
 alias xm="xmake"
 alias xmr="xmake build && xmake run"
 alias sdrnow="shutdown -r now"
 alias sdnow="shutdown now"
+alias code=/mnt/c/Users/Rossetta/AppData/Local/Programs/Microsoft\\\ VS\\\ Code/code.exe
+
 
 # alacritty colorscheme
 # 准备工作: pip install alacritty-colorscheme
