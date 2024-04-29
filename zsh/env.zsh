82fae1b8ff65fc858cbab2c5834ddb071e9c421b
Rossettaylm <2441197035@qq.com>
2024-04-29 12:14:55 +0800

    empty

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -3,15 +3,14 @@
 # If you come from bash you might have to change your $PATH.
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 #
-export TOMCAT_HOME="/usr/share/tomcat8"
 export GITHUB_ACCOUNT_PREFIX="https://github.com/Rossettaylm"
 export SCRIPTS_HOME="$HOME/.config/scripts"
 
 # Path to your oh-my-zsh installation.
 export ZSH_HOME="$HOME/.config/zsh"
 export ZSH="$ZSH_HOME/oh-my-zsh"
-export FZF_HOME="$HOME/softwares/fzf"
 export SOFTWARES_HOME="$HOME/softwares"
+export FZF_HOME="$SOFTWARES_HOME/fzf"
 
 export PATH=$SCRIPTS_HOME:$PATH
 export PATH=$HOME/.local/bin:$PATH
